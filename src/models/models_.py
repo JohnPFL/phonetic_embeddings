af2661463ae1238112b28dfe0c816fb371bbf52b
GS <ytdossier@gmail.com>
2023-08-31 19:12:40 +0200

    phonetic embs added

--- src/models/models_.py
+++ src/models/models_.py
@@ -1,27 +1,13 @@
+
 from abc import ABC, abstractmethod
-import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer # type: ignore
-from sklearn.metrics import f1_score, recall_score, precision_score # type: ignore
-from sklearn.model_selection import train_test_split # type: ignore 
 from sklearn.svm import LinearSVC # type: ignore
-from torch.utils.data import DataLoader
-from data.data import TextDataset, NeuralDataset
-from data import data 
-from module.architectures import CNNbase
-from utils.util import CharVocabulary
+from module.architectures import CNNbase, PhoneticCNN
+from utils.util import CharVocabulary, PhoneticCharVocabulary
 import torch
-import torch.nn as nn
-from tqdm import tqdm
-from utils.util import EarlyStop
-import pandas as pd
-import os
 import sys
-import shutil
-import time
 from os.path import join
-import torch.nn.functional as F
-import torch.nn.utils as utils
-from classification_functions import train_model, predict_samples
+from models.classification_functions import train_model, predict_samples
 
 
 class Model(ABC):
@@ -53,25 +39,32 @@ class SVMclassifier(Model):
     
 class CNNbaseClassifier(Model):
 
-    def __init__(self, batch_size, learning_rate, seed, model_name, patience=False, nepochs=500, device='cuda', **kwargs):
+    def __init__(self, batch_size, learning_rate, seed, 
+                 model_name, patience=0, nepochs=500, device='cuda', paths = {'tmp_path': 'tmp', 
+                                                       'def_path': 'def'}, **kwargs):
         self.batch_size = batch_size
         self.nepochs = nepochs
         self.device = device
         self.model_name = model_name
         self.patience = patience
-        self.kwargs = kwargs
         self.name = 'cnn'
         self.learning_rate = learning_rate
         # seed for the validation split
         self.seed = seed
+        self.paths = paths
+        self.kwargs = kwargs
+
         
     def fit(self, X, y, datasetname):
         self.char_encoder = CharVocabulary(X)
         vocab_length = len(self.char_encoder)+1
-        self.model = CNNbase(vocab_length, **self.kwargs).to(self.device).xavier_uniform()
+        self.model = CNNbase(vocab_length, **self.kwargs).to(self.device)
         self.latest_model = train_model(self.model, X, y, self.char_encoder, datasetname,
-                                        self.batch_size, self.seed, nepochs=self.nepochs, model_name=self.model_name, 
-                                        patience= self.patience, lazy_loader=True, device=self.device, lr=self.learning_rate)
+                                        self.batch_size, self.seed, nepochs=self.nepochs, 
+                                        model_name=self.model_name, 
+                                        patience= self.patience, lazy_loader=True, 
+                                        device=self.device, lr=self.learning_rate,
+                                        tmp_path= self.paths['tmp_path'], def_path= self.paths['def_path'])
 
     def predict(self, X):
         if self.patience:
@@ -80,3 +73,47 @@ class CNNbaseClassifier(Model):
             return predict_samples(model, X, self.char_encoder, self.batch_size, lazy_loader=True, device=self.device)
         else:
             return predict_samples(self.model, X, self.char_encoder, self.batch_size, lazy_loader=True, device=self.device)
+
+
+class CNNPhoneticClassifier(Model):
+
+    def __init__(self, batch_size, phonetic_dataset, 
+                 save_folder,
+                  learning_rate, seed, 
+                 model_name = 'False', 
+                 patience = 0, nepochs = 500, paths = {'tmp_path': 'tmp', 
+                                                       'def_path': 'def'}, **kwargs):
+        self.batch_size = batch_size
+        self.nepochs = nepochs
+        self.device = 'cuda'
+        self.model_name = model_name
+        self.patience = patience
+        self.kwargs = kwargs
+        self.name = 'phonetic_cnn'
+        self.learning_rate = learning_rate
+        self.phonetic_dataset = phonetic_dataset
+        self.save_folder = save_folder
+        # seed for the validation split
+        self.seed = seed 
+        self.paths = paths
+
+    def fit(self, X, y, datasetname):
+        # Fit the classifier to the training data
+        self.char_encoder = PhoneticCharVocabulary(X, self.phonetic_dataset, self.save_folder)
+        vocab_length = len(self.char_encoder)+1
+        self.model = PhoneticCNN(vocab_length, **self.kwargs).to('cuda')
+        self.latest_model = train_model(self.model, X, y, self.char_encoder, 
+                                        datasetname, self.batch_size, self.seed, 
+                                        nepochs= self.nepochs,
+                                        model_name=self.model_name, patience=self.patience, 
+                                        lazy_loader=True, device=self.device, lr=self.learning_rate,
+                                        tmp_path= self.paths['tmp_path'], def_path= self.paths['def_path'])
+    
+    def predict(self, X):
+        if self.patience:
+            print(f'latest best model is... {self.latest_model =}')
+            model = torch.load(self.latest_model)
+            return predict_samples(model, X, self.char_encoder, self.batch_size, lazy_loader=True, device=self.device)
+        else:
+            return predict_samples(self.model, X, self.char_encoder, self.batch_size, lazy_loader=True, device=self.device)
+        
